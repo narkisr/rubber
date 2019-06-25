87f4d45cf1051392711df98cca2d5e8a4da861c0
ronen <narkisr@gmail.com>
2019-06-25 23:55:25 +1000

    removing eastwood

--- project.clj
+++ project.clj
@@ -35,7 +35,6 @@
     }
 
    :plugins [
-     [jonase/eastwood "0.2.4"]
      [lein-cljfmt "0.5.6"]
      [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
      [lein-tag "0.1.0"]
@@ -50,7 +49,7 @@
 
    :aliases {
      "travis" [
-        "do" "clean," "compile," "cljfmt" "check," "eastwood," "test"
+        "do" "clean," "compile," "cljfmt" "check," "test"
      ]
    }
 
